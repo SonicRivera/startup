5dcbef620c672bf9815d4b13b3f1756f5c807aff
SonicRivera <connerrivera@gmail.com>
2024-11-09 21:48:52 -0700

    Added placeholder recipes

--- src/components/RecipeProvider.jsx
+++ src/components/RecipeProvider.jsx
@@ -14,7 +14,40 @@ export const RecipeProvider = ({ children }) => {
             category: "Main Course",
             rating: 4,
             image: "https://images.pexels.com/photos/5638539/pexels-photo-5638539.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-        }
+        },
+        {
+            name: "Chicken Tikka Masala",
+            ingredients: "Chicken, Tikka, Masala",
+            instructions: "Cook it.",
+            prepTime: "15 mins",
+            cookTime: "15 mins",
+            servings: 4,
+            category: "Main Course",
+            rating: 3,
+            image: "https://images.pexels.com/photos/27287005/pexels-photo-27287005/free-photo-of-a-plate-of-food-with-rice-and-vegetables-on-it.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        },
+        {
+            name: "Chow-mein Stir Fry",
+            ingredients: "Chow mein, Stir, Fry",
+            instructions: "Cook it.",
+            prepTime: "30 mins",
+            cookTime: "15 mins",
+            servings: 2,
+            category: "Main Course",
+            rating: 4,
+            image: "https://images.pexels.com/photos/18698263/pexels-photo-18698263/free-photo-of-food-photography.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        },
+        {
+            name: "Spaghetti Carbonara",
+            ingredients: "Spaghetti, carbonara",
+            instructions: "Cook it.",
+            prepTime: "35 mins",
+            cookTime: "10 mins",
+            servings: 8,
+            category: "Main Course",
+            rating: 5,
+            image: "https://images.pexels.com/photos/26597663/pexels-photo-26597663/free-photo-of-close-up-of-pasta-with-meat.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        },
     ]);
 
     const addRecipe = (recipe) => {
