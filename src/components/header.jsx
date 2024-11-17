5460d8146414d79c14d7fd04938b5086723b08a4
SonicRivera <connerrivera@gmail.com>
2024-11-16 18:02:32 -0700

    API Debugging

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,7 @@ export function Header({ username }) {
 
   const handleLogout = () => {
     localStorage.removeItem('token');
-    navigate('/login');
+    navigate('/');
     location.reload();
   };
 
